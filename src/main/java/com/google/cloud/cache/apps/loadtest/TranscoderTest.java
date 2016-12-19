d3d44eec52b867791c19ccf272f99e4c80b3b6ab
Bin Yu <yubin154@gmail.com>
2016-12-19 13:54:45 -0800

    add test case for byte, short

--- src/main/java/com/google/cloud/cache/apps/loadtest/TranscoderTest.java
+++ src/main/java/com/google/cloud/cache/apps/loadtest/TranscoderTest.java
@@ -21,16 +21,10 @@ public final class TranscoderTest extends SpyMemcachedBaseTest {
     testBoolean();
     testInteger();
     testLong();
+    testShort();
+    testByte();
+    testSerializableObject();
     testBytes();
-  }
-
-  public void testBytes() throws Exception {
-    checkValuesInBothClients("zero byte", new byte[0]);
-    byte[] data = new byte[] {1, 2, 3, 4, 5, 6};
-    checkValuesInBothClients("bytes", data);
-    data = new byte[] {65, 66, 67, 0, 68, 69, 70};
-    checkValuesInBothClients("someBytesWithNull", data);
-    //checkValuesInBothClients("bigKeyBytes", Strings.repeat("x", 300).getBytes());
   }
 
   public void testStr() throws Exception {
@@ -48,19 +42,35 @@ public final class TranscoderTest extends SpyMemcachedBaseTest {
     checkValuesInBothClients("integer zero", new Integer(0));
     checkValuesInBothClients("integer max", Integer.MAX_VALUE);
     checkValuesInBothClients("integer min", Integer.MIN_VALUE);
-    checkValuesInBothClients("integer max overflow", Integer.MAX_VALUE+1);
-    checkValuesInBothClients("integer min overflow", Integer.MIN_VALUE-1);
+    checkValuesInBothClients("integer max overflow", Integer.MAX_VALUE + 1);
+    checkValuesInBothClients("integer min overflow", Integer.MIN_VALUE - 1);
   }
 
   public void testLong() throws Exception {
     checkValuesInBothClients("long zero", new Long(0));
     checkValuesInBothClients("long max", Long.MAX_VALUE);
     checkValuesInBothClients("long min", Long.MIN_VALUE);
-    checkValuesInBothClients("long max overflow", Long.MAX_VALUE+1);
-    checkValuesInBothClients("long min overflow", Long.MIN_VALUE-1);
+    checkValuesInBothClients("long max overflow", Long.MAX_VALUE + 1);
+    checkValuesInBothClients("long min overflow", Long.MIN_VALUE - 1);
   }
 
-  public void testObject() throws Exception {
+  public void testShort() throws Exception {
+    checkValuesInBothClients("short zero", new Short("0"));
+    checkValuesInBothClients("short max", Short.MAX_VALUE);
+    checkValuesInBothClients("short min", Short.MIN_VALUE);
+    checkValuesInBothClients("short max overflow", Short.MAX_VALUE + 1);
+    checkValuesInBothClients("short min overflow", Short.MIN_VALUE - 1);
+  }
+
+  public void testByte() throws Exception {
+    checkValuesInBothClients("byte zero", (byte) 0);
+    checkValuesInBothClients("byte max", Byte.MAX_VALUE);
+    checkValuesInBothClients("byte min", Byte.MIN_VALUE);
+    checkValuesInBothClients("byte max overflow", Byte.MAX_VALUE + 1);
+    checkValuesInBothClients("byte min overflow", Byte.MIN_VALUE - 1);
+  }
+
+  public void testSerializableObject() throws Exception {
     java.util.Date dateObj = new java.util.Date();
     checkValuesInBothClients("date", dateObj);
     java.util.ArrayList listObj = new java.util.ArrayList();
@@ -69,20 +79,29 @@ public final class TranscoderTest extends SpyMemcachedBaseTest {
     checkValuesInBothClients("map", mapObj);
   }
 
+  public void testBytes() throws Exception {
+    checkValuesInBothClients("zero byte", new byte[0]);
+    byte[] data = new byte[] {1, 2, 3, 4, 5, 6};
+    checkValuesInBothClients("bytes", data);
+    data = new byte[] {65, 66, 67, 0, 68, 69, 70};
+    checkValuesInBothClients("someBytesWithNull", data);
+    //checkValuesInBothClients("bigKeyBytes", Strings.repeat("x", 300).getBytes());
+  }
+
   private void checkValuesInBothClients(String testDesc, Object obj) throws Exception {
     result.append(
-        String.format("%s, class=%s, value=%s\n", testDesc, obj.getClass(), obj.toString()));
+        String.format("Testing %s, %s=%s\n", testDesc, obj.getClass().getName(), obj.toString()));
     String key1 = randomKey();
-    result.append(String.format("key=%s, keyg=%s", key1, makeKey(key1)));
+    result.append(String.format("d2g key=%s\n", key1));
     // add by memcached, retrieved from memcacheg
-    expectTrue(client.add(makeKey(key1), DEFAULT_EXP, obj).get(), "add d2g");
-    expectEqual(obj, aeClient.get(key1), "value verified");
+    expectTrue(client.add(makeKey(key1), DEFAULT_EXP, obj).get(), "PUT");
+    expectEqual(obj, aeClient.get(key1), "GET verified");
 
     String key2 = randomKey();
-    result.append(String.format("key=%s, keyg=%s", key2, makeKey(key2)));
+    result.append(String.format("g2d key=%s\n", key2));
     // add by memcacheg, retrieved from memcached
     aeClient.put(key2, obj);
-    expectEqual(obj, aeClient.get(key2), "add g2d");
-    expectEqual(obj, client.get(makeKey(key2)), "value verified");
+    expectEqual(obj, aeClient.get(key2), "PUT");
+    expectEqual(obj, client.get(makeKey(key2)), "GET verified");
   }
 }
